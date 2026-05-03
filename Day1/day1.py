77284669d036491184712481c31b307e45b3204c
FlorianxK <Florian.Koehler@jokonet.de>
2026-05-03 17:09:50 +0200

    Initial commit

---
+++ Day1/day1.py
@@ -0,0 +1,21 @@
+from typing import *
+
+def dayOne():
+    #read
+    with open("Day1/1.txt") as file:
+        pass
+
+def dayOne2():
+    pass
+
+def dayOne3():
+    pass
+
+def main():
+    print("Hallo")
+    print(dayOne(), "ist die Lösung von Teil 1")
+    print(dayOne2(), "ist die Lösung von Teil 2")
+    print(dayOne3(), "ist die Lösung von Teil 3")
+
+if __name__=="__main__":
+    main()
